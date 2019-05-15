c3126ac33a4718480a592c86fb477b9b497b5296
Filip Questier <filip@questier.be>
2019-05-15 21:20:48 +0200

    Committed initial version.

---
+++ Heemkunde.AspNetCore.WebApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Heemkunde.AspNetCore.WebApp.Models;
+
+namespace Heemkunde.AspNetCore.WebApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ Heemkunde.AspNetCore.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Heemkunde.AspNetCore.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
